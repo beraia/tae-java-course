95bf3be9ccec51ef14130e02c5df147acc2fa98b
beraia <temuriberaia1@gmail.com>
2026-05-28 20:56:29 +0400

    [Temur Beraia] HW4 — EvenOddCounter - Add even and odd number lists to output

--- src/main/java/homework4/EvenOddCounter.java
+++ src/main/java/homework4/EvenOddCounter.java
@@ -7,15 +7,20 @@ public class EvenOddCounter {
         int oddCounter = 0;
         int evenCounter = 0;
 
+        String odds = "";
+        String evens = "";
+
         for(int i=1; i<=lastTwoDigits; i++){
             if(i % 2 == 0){
                 evenCounter++;
+                evens += i + " ";
             }
             else{
                 oddCounter++;
+                odds += i + " ";
             }
         }
-        System.out.println("Odd numbers count " + oddCounter);
-        System.out.println("Even numbers count " + evenCounter);
+        System.out.println("Odd " + oddCounter + " (" + odds.trim() + ")");
+        System.out.println("Even " + evenCounter + " (" + evens.trim() + ")");
     }
 }
