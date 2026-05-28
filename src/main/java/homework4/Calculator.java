a1ae3a7483943e030ce8a0fb02d36ff87461b389
beraia <temuriberaia1@gmail.com>
2026-05-28 19:45:20 +0400

    [Temur Beraia] HW4 — Calculator

---
+++ src/main/java/homework4/Calculator.java
@@ -0,0 +1,23 @@
+package homework4;
+
+public class Calculator {
+    static void main(String[] args) {
+        double firstNumber = 10;
+        double secondNumber = 2;
+        char operator = '/';
+
+        switch (operator){
+            case '+' -> System.out.println("Result: " + (firstNumber + secondNumber));
+            case '-' -> System.out.println("Result: " + (firstNumber - secondNumber));
+            case '*' -> System.out.println("Result: " + (firstNumber * secondNumber));
+            case '/' -> {
+                if(secondNumber == 0){
+                    System.out.println("Cannot divide by zero");
+                }else{
+                    System.out.println("Result: "+ (firstNumber / secondNumber));
+                }
+            }
+            default -> System.out.println("Unknown operation");
+        }
+    }
+}
