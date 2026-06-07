7be6393a157796a7c55521afb06806c0d887652d
beraia <temuriberaia1@gmail.com>
2026-06-07 13:32:43 +0400

    [Temur Beraia] HW6 - BankAccount

--- src/main/java/homework6/Main.java
+++ src/main/java/homework6/Main.java
@@ -3,14 +3,18 @@ package homework6;
 
 public class Main {
     public static void main(String[] args) {
-        Person student = new Person("თემო", 29, "temuriberaia1@gmail.com");
-        Person p1 = new Person("ანა", 24, "ana24@gmail.com");
+//        HW6 - Person
+//        Person student = new Person("თემო", 29, "temuriberaia1@gmail.com");
+//        Person p1 = new Person("ანა", 24, "ana24@gmail.com");
+//
+//        System.out.println(student.getName());
+//        student.introduce();
+//        System.out.println(p1.getName());
+//        p1.introduce();
 
-        System.out.println(student.getName());
-        student.introduce();
-        System.out.println(p1.getName());
-        p1.introduce();
-
-
+//        HW6 - BankAccount
+        BankAccount acc = new BankAccount("Temo", 100);
+        acc.deposit(50);
+        System.out.println("ბალანსი არის : " + acc.getBalance());
     }
 }
