99968d4ef1c54ff0e1b230c83c8cd5f9ce8a94e7
beraia <temuriberaia1@gmail.com>
2026-06-14 15:41:05 +0400

    [Temur Beraia] HW8 — InvalidAgeException + AgeValidator

---
+++ src/main/java/homework8/InvalidAgeException.java
@@ -0,0 +1,8 @@
+/** @author <Temur Beraia> */
+package homework8;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message){
+        super(message);
+    }
+}
